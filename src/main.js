6bc7fd155e46b63b4875362f24c3c3774f121d6c
l09250701@gmail.com <l09250701@gmail.com>
2024-03-15 20:20:41 +0800

    main.js備註

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
+//樣式
 import './assets/main.css'
-
+//套件
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+//引入主程式
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+//設定全域的網站名稱
 app.use(createPinia())
 app.use(router)
 
