ce836395b29d3505d9173b8cf6b0c5a2074a1a4d
l09250701@gmail.com <l09250701@gmail.com>
2024-03-25 01:35:46 +0800

    footer完成80% 剩backtotop

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 
 
 <template>
-
   <MainHeader />
+  <MainFooter />
+
 </template>
 
 <script>
 import { RouterView } from 'vue-router'
 import MainHeader from "./components/MainHeader.vue";
+import MainFooter from "./components/MainFooter.vue";
 
 export default {
 
   components:{
     MainHeader,
+    MainFooter,
     RouterView,
   }
 }
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="footer">
+        <div class="footerWrapper">
+            <div class="footerLeft">
+                <div class="footerlogo">
+                    <img src="../assets/images/footerLOGO.svg" alt="footerlogo">
+                    <div class="footericon">
+                        <a href="https://www.facebook.com/today100points" target="_blank">
+                            <img src="../assets/images/grayFB.svg" alt="fb_icon" class="svg-icon">
+                        </a>
+                        <a href="https://www.instagram.com/100points_today_tw?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+                            target="_blank">
+                            <img src="../assets/images/grayIG.svg" alt="ig_icon" class="svg-icon">
+                        </a>
+                    </div>
+                </div>
+                <ul class="footercontent">
+                    <li>營業時間：09:00~20:00</li>
+                    <li>連絡電話：03-4518159</li>
+                    <li>店鋪地址：桃園市中壢區成章四街94號</li>
+                </ul>
+            </div>
+            <div class="footerRight">
+                <p class="backToTop">back to top</p>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>COPYRIGHT © 2023 今天100分 - ALL RIGHTS RESERVED.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+};
+</script>
